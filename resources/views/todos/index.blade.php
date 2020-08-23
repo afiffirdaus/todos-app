ab98bcf359483a4e192870a3b918f8b2839633d4
afiffirdaus <afifmuhdafif@gmail.com>
2020-08-24 02:00:54 +0800

    todos

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -8,18 +8,37 @@
     <title>Todos</title>
 </head>
 <body>
-    <h1 class="text-center">TODOS PAGE</h1>
-
+    <h1>TODOS PAGE</h1>
     <ul>
-            @foreach($todos as $todo)
-
-                <li>
-                    {{ $todo->name}}
-                </li>
-
-            @endforeach
+      @foreach($todos as $todo)
+        <li>
+          {{ $todo->name }}
+        </li>
+      @endforeach
     </ul>
-    
-
-</body>
-</html>
+    <div class="container">
+      <h1 class="text-center my-5">TODOS PAGE</h1>
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="card card-default">
+            <div class="card-header">
+              Todos
+            </div>
+  
+            <div class="card-body">
+              <ul class="list-group">
+                @foreach($todos as $todo)
+                  <li class="list-group-item">
+                    {{ $todo->name }}
+  
+                    <button class="btn btn-primary btn-sm float-right">View</button>
+                  </li>
+                @endforeach
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+  </html>
